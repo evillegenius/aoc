861333a9980ea41594b8e5e49be68b71ed4d2880
Jim Atkinson <j_git@atkinsons.net>
2024-12-05 22:36:35 -0800

    Day06 solution.

--- 2024/06/Day06.py
+++ 2024/06/Day06.py
@@ -2,14 +2,32 @@
 """
 <Problem description here>
 """
-import sys
-import re
 import numpy as np
+from collections import namedtuple
+
+class Point(namedtuple('Point', ['row', 'col'])):
+    __slots__ = ()
+    def __add__(self, other):
+        return Point(self.row + other.row, self.col + other.col)
+    def __str__(self):
+        return f'Point({self.row}, {self.col})'
+    
+# direction for initial map location
+dirs = {2: Point(-1, 0),
+        3: Point(0, 1),
+        4: Point(1, 0),
+        5: Point(0, -1)}
+# when we encounter an obstacle, this is our new direction.
+rotate = {Point(-1, 0): Point(0, 1),
+          Point(0, 1): Point(1, 0),
+          Point(1, 0): Point(0, -1),
+          Point(0, -1): Point(-1, 0)}
 
 class Day06:
     def __init__(self):
         self.input = None
 
+        self.content = None
         self.lines = []
         self.grid = None
         
@@ -27,11 +45,12 @@ class Day06:
 
     def ParseInput(self):
         with open(self.input) as input:
-            self.lines = input.read().strip().split('\n')
+            self.contents = input.read().strip()
+        self.lines = self.contents.split('\n')
 
         ########################################################################
         # If the puzzle is not grid/map based, delete these lines.
-        gridKey = {'.': 0, '#': 1, 'O': 2}
+        gridKey = {'.': 0, '#': 1, '^': 2, '>': 3, 'v': 4, '<': 5}
         self.height = len(self.lines)
         self.width = len(self.lines[0])
 
@@ -44,11 +63,56 @@ class Day06:
 
 
     def Part1(self):
-        answer = 0
+        coords = np.argwhere(self.grid > 1)
+        pos = Point(*coords[0])
+        delta = dirs[self.grid[pos]]
+        visited = set()
+        while True:
+            visited.add(pos)
+            newPos = pos + delta
+            if not ((0 <= newPos.row < self.height) and (0 <= newPos.col < self.width)):
+                break
+            if self.grid[newPos] == 1:
+                delta = rotate[delta]
+                continue
+            pos = newPos
+
+        answer = len(visited)
+
         return answer
 
     def Part2(self):
         answer = 0
+        coords = np.argwhere(self.grid > 1)
+        startPos = Point(*coords[0])
+        startDelta = dirs[self.grid[startPos]]
+        results = []
+        for obsRow in range(0, self.height):
+            for obsCol in range(0, self.width):
+                obsPoint = Point(obsRow, obsCol)
+                if self.grid[obsPoint] != 0:
+                    continue
+                self.grid[obsPoint] = 1
+                pos = startPos
+                delta = startDelta
+                visited = set()  # Set of (pos, delta)
+
+                while True:
+                    visited.add((pos, delta))
+                    newPos = pos + delta
+                    if not ((0 <= newPos.row < self.height) and (0 <= newPos.col < self.width)):
+                        break
+                    if self.grid[newPos] == 1:
+                        newPos = pos
+                        delta = rotate[delta]
+                    if (newPos, delta) in visited:
+                        # We found a loop. Record it.
+                        answer += 1
+                        print(f'Loop: {obsPoint}')
+                        break
+                    pos = newPos
+                self.grid[obsPoint] = 0
+
         return answer
     
 if __name__ == '__main__':
