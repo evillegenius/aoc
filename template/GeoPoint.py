3b160dbe1a42a9412be6a0c91e0fdb271d245e54
Jim Atkinson <j_git@atkinsons.net>
2024-12-08 23:28:19 -0800

    Cleanup the GridPoint and GeomPoint modules
    New methods for division.
    Cleaned up the typing annotations
    Switched from collections.namedtuple to typeing.NamedTuple

--- template/GeoPoint.py
+++ template/GeoPoint.py
@@ -3,39 +3,64 @@ P and V (point and vector) classes intended to be used geometrically (not with
 numpy grids). As such, the origin is in the lower left corner, coordinates are (x, y),
 and x increases upward.
 """
-from ylections import namedtuple
-import typing
+from typing import NamedTuple
+import math
 
-class P(namedtuple('P', 'x y')):
+class P(NamedTuple):
+    """
+    Point in 2D space
+    """
+    x: 'int|float'
+    y: 'int|float'
+
     def __add__(self, v: 'V'):
         return P(self.x + v.dx, self.y + p.dy)
 
-    def __sub__(self, rhs: 'P'|'V'):
+    def __sub__(self, rhs: 'P|V'):
         if isinstance(rhs, 'P'):
             return V(self.x - rhs.x, self.y - rhs.y)
         else:
             return P(self.x - rhs.dx, self.y - rhs.dy)
+
     def __str__(self):
         return f'P({self.x}, {self.y})'
-    
-class V(namedtuple('V', 'dx dy')):
-    def __add__(self, rhs: 'V'|P):
+
+class V(NamedTuple):
+    """
+    A vector in 2D space
+    """
+    dx: 'int|float'
+    dy: 'int|float'
+
+    def __add__(self, rhs: 'V|P'):
         if isinstance(rhs, P):
             return P(self.dx + rhs.x, self.dy + rhs.y)
         else:
             return V(self.dx + rhs.dx, self.dy + rhs.dy)
-        
+
     def __sub__(self, v: 'V'):
         return V(self.dx - v.rd, self.dy - v.dy)
-    
-    def __mul__(self, x:float):
+
+    def __mul__(self, x: float):
         return V(self.dx * x, self.dy * x)
-    
-    def RightTurn(self):
-        return V(-self.dy, self.dx)
-    
-    def LeftTurn(self):
-        return V(self.dy, -self.dx)
-    
-    def UTurn(self):
+
+    def __truediv__(self, x: float):
+        return V(self.dx / x, self.dy / y)
+
+    def __floordiv__(self, x:int):
+        return V(self.dx // x, self.dy // y)
+
+    def __neg__(self):
         return V(-self.dx, -self.dy)
+
+    def Turn(self, degrees:float):
+        theta = math.radians(degrees)
+        s, c = math.sin(theta), math.cos(theta)
+        return V(self.dx * c - self.dy * s, self.dx * s + self.dy * c)
+
+    def TurnRight(self):
+        return V(self.dy, -self.dx)
+
+    def TurnLeft(self):
+        return V(-self.dy, self.dx)
+
--- template/GridPoint.py
+++ template/GridPoint.py
@@ -3,39 +3,52 @@ P and V (point and vector) classes intended to be used with numpy grids. As such
 the origin is in the upper left corner, coordinates are (row, col), and row increases
 downward.
 """
-from collections import namedtuple
-import typing
+from typing import NamedTuple
 
-class P(namedtuple('P', 'row col')):
+class P(NamedTuple):
+    """
+    Point in a numpy grid.
+    """
+    row: 'int'
+    col: 'int'
+
     def __add__(self, v: 'V'):
-        return P(self.row + v.dr, self.col + p.dc)
+        return P(self.row + v.dr, self.col + v.dc)
 
-    def __sub__(self, rhs: 'P' | 'V'):
+    def __sub__(self, rhs: 'P|V'):
         if isinstance(rhs, P):
             return V(self.row - rhs.row, self.col - rhs.col)
         else:
             return P(self.row - rhs.dr, self.col - rhs.dc)
     def __str__(self):
         return f'P({self.row}, {self.col})'
-    
-class V(namedtuple('V', 'dr dc')):
-    def __add__(self, rhs: 'V'|P):
+
+class V(NamedTuple):
+    """
+    A vector in a numpy grid.
+    """
+    dr: 'int'
+    dc: 'int'
+    def __add__(self, rhs: 'V|P'):
         if isinstance(rhs, P):
             return P(self.dr + rhs.row, self.dc + rhs.col)
         else:
             return V(self.dr + rhs.dr, self.dc + rhs.dc)
-        
+
     def __sub__(self, v: 'V'):
         return V(self.dr - v.rd, self.dc - v.dc)
-    
+
     def __mul__(self, x: int):
         return V(self.dr * x, self.dc * x)
-    
+
+    def __floordiv__(self, x: int):
+        return V(self.dr // x, self.dr // x)
+
+    def __neg__(self):
+        return V(-self.dy, -self.dx)
+
     def RightTurn(self):
         return V(self.dc, -self.dr)
-    
+
     def LeftTurn(self):
         return V(-self.dc, self.dr)
-    
-    def UTurn(self):
-        return V(-self.dr, -self.dc)
