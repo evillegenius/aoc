d3d87179fef4ec547abb5533cdb5ad2d67fe18a7
Jim Atkinson <j_git@atkinsons.net>
2024-12-08 23:24:25 -0800

    Day09 improved solution
    Does not build a big table of blocks, it just messes about with the
    indices of the files.

--- 2024/09/Day09.py
+++ 2024/09/Day09.py
@@ -2,9 +2,6 @@
 """
 <Problem description here>
 """
-import sys
-import re
-import numpy as np
 
 class Day09:
     def __init__(self):
@@ -36,37 +33,40 @@ class Day09:
 
     def Part1(self):
         answer = 0
-        files = [[id, used, empty] for id, used, empty in self.files]
-        totalUsed = sum(used for _, used, _ in files)
-        blocks = [-1] * totalUsed
-        i, j = 0, len(files) - 1
-        b = 0
-        jId, jUsed, jEmpty = files[j]
-        while i < j:
-            iId, iUsed, iEmpty = files[i]
-            blocks[b:b+iUsed] = [iId] * iUsed
-            b += iUsed
-            while iEmpty > 0:
-                if iEmpty >= jUsed:
-                    blocks[b:b+jUsed] = [jId] * jUsed
-                    b += jUsed
-                    iEmpty -= jUsed
-                    files[j] = [0, 0, jEmpty + jUsed]
-                    j -= 1
-                    jId, jUsed, jEmpty = files[j]
-                else:
-                    blocks[b:b+iEmpty] = [jId] * iEmpty
-                    b += iEmpty
-                    jUsed -= iEmpty
-                    files[j] = [jId, jUsed, jEmpty + iEmpty]
-                    iEmpty = 0
-            i += 1
-        b = len(blocks) - 1
-        while blocks[b] == -1:
-            blocks[b] = jId
-            b -= 1
-        for b, id in enumerate(blocks):
-            answer += b * id
+        files = [[0, id, used, empty] for id, used, empty in self.files]
+        index = 0
+        for file in files:
+            file[0] = index
+            index += file[2] + file[3]
+
+        i = 0
+        while i < len(files):
+            iIndex, iId, iUsed, iEmpty = files[i]
+            jIndex, jId, jUsed, jEmpty = files[-1]
+
+            if iEmpty == 0:
+                i += 1
+                continue
+            if iEmpty >= jUsed:
+                # Move the file
+                files.insert(i + 1, [iIndex + iUsed, jId, jUsed, iEmpty - jUsed])
+                files.pop()
+                iEmpty = 0
+                files[i][3] = iEmpty
+                i += 1  # point to the newly moved file.
+            else:
+                # Split the file
+                jUsed -= iEmpty
+                jEmpty += iEmpty
+                files[-1] = [jIndex, jId, jUsed, jEmpty]
+                files.insert(i + 1, [iIndex + iUsed, jId, iEmpty, 0])
+                iEmpty = 0
+                files[i][3] = iEmpty
+                i += 2  # skip the newly inserted shard, it has no unused space.
+
+        for index, id, used, empty in files:
+            for b in range(index, index + used):
+                answer += b * id
 
         return answer
 
