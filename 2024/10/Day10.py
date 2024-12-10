58b4f8d666717ae4ba01be7f0d610d803c979c38
Jim Atkinson <j_git@atkinsons.net>
2024-12-09 21:46:50 -0800

    Day10 solution.

--- 2024/10/Day10.py
+++ 2024/10/Day10.py
@@ -2,9 +2,69 @@
 """
 <Problem description here>
 """
-import sys
-import re
 import numpy as np
+
+"""
+P and V (point and vector) classes intended to be used with numpy grids. As such
+the origin is in the upper left corner, coordinates are (row, col), and row increases
+downward.
+"""
+from typing import NamedTuple
+
+class P(NamedTuple):
+    """
+    Point in a numpy grid.
+    """
+    row: 'int'
+    col: 'int'
+
+    def __add__(self, v: 'V'):
+        return P(self.row + v.dr, self.col + v.dc)
+
+    def __sub__(self, rhs: 'P|V'):
+        if isinstance(rhs, P):
+            return V(self.row - rhs.row, self.col - rhs.col)
+        else:
+            return P(self.row - rhs.dr, self.col - rhs.dc)
+    def __str__(self):
+        return f'P({self.row}, {self.col})'
+
+class V(NamedTuple):
+    """
+    A vector in a numpy grid.
+    """
+    dr: 'int'
+    dc: 'int'
+    def __add__(self, rhs: 'V|P'):
+        if isinstance(rhs, P):
+            return P(self.dr + rhs.row, self.dc + rhs.col)
+        else:
+            return V(self.dr + rhs.dr, self.dc + rhs.dc)
+
+    def __sub__(self, v: 'V'):
+        return V(self.dr - v.rd, self.dc - v.dc)
+
+    def __mul__(self, x: int):
+        return V(self.dr * x, self.dc * x)
+
+    def __floordiv__(self, x: int):
+        return V(self.dr // x, self.dr // x)
+
+    def __neg__(self):
+        return V(-self.dy, -self.dx)
+
+    def RightTurn(self):
+        return V(self.dc, -self.dr)
+
+    def LeftTurn(self):
+        return V(-self.dc, self.dr)
+
+UP = V(-1, 0)
+DOWN = V(1, 0)
+LEFT = V(0, -1)
+RIGHT = V(0, 1)
+
+directions = (UP, DOWN, LEFT, RIGHT)
 
 class Day10:
     def __init__(self):
@@ -27,28 +87,65 @@ class Day10:
 
     def ParseInput(self):
         with open(self.input) as input:
-            self.lines = input.read().strip().split('\n')
+            self.contents = input.read().strip()
+
+        self.lines = self.contents.split('\n')
 
         ########################################################################
         # If the puzzle is not grid/map based, delete these lines.
-        gridKey = {'.': 0, '#': 1, 'O': 2}
         self.height = len(self.lines)
         self.width = len(self.lines[0])
 
         self.grid = np.zeros((self.height, self.width), dtype=int)
         for row, line in enumerate(self.lines):
             for col, ch in enumerate(line):
-                self.grid[row, col] = gridKey[ch]
+                self.grid[row, col] = int(ch)
         #
         ########################################################################
 
 
+    def dfs(self, start, found):
+        score = 0
+        altitude = self.grid[start]
+        if altitude == 9:
+            if found is None:
+                return 1
+            if start in found:
+                return 0
+            found.add(start)
+            return 1
+
+        for delta in directions:
+            pos = start + delta
+            if (not (0 <= pos.row < self.height and 0 <= pos.col < self.width)):
+                continue
+            if self.grid[pos] == altitude + 1:
+                score += self.dfs(pos, found)
+
+        return score
+        
     def Part1(self):
         answer = 0
+        starts = [P(*item) for item in np.argwhere(self.grid == 0)]
+        for start in starts:
+            found = set()
+            score = self.dfs(start, found)
+            #print(f'{start = }, {score = }')
+
+            answer += score
+
         return answer
 
     def Part2(self):
         answer = 0
+        starts = [P(*item) for item in np.argwhere(self.grid == 0)]
+        for start in starts:
+            found = set()
+            score = self.dfs(start, None)
+            #print(f'{start = }, {score = }')
+
+            answer += score
+
         return answer
     
 if __name__ == '__main__':
