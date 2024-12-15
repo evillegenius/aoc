5d37380def1748184a107108e302ed416c140678
Jim Atkinson <j_git@atkinsons.net>
2024-12-14 22:55:18 -0800

    Update GeoPoint and GridPoint modules
    Added __mod__ method to take the components of point mod components of a vector
    
    Add __rmul__ to vector classes so that (10 * v) == (v * 10)
    
    Add __abs__ to return the length of a vector.
    
    Extended __mul__ so that vector * vector returns a dot product
    
    Added __xor__ as a cross product
    
    Renamed TurnRight and TurnLeft to Right and Left respectively

--- template/GeoPoint.py
+++ template/GeoPoint.py
@@ -17,10 +17,14 @@ class P(NamedTuple):
         return P(self.x + v.dx, self.y + p.dy)
 
     def __sub__(self, rhs: 'P|V'):
-        if isinstance(rhs, 'P'):
+        if isinstance(rhs, P):
             return V(self.x - rhs.x, self.y - rhs.y)
         else:
             return P(self.x - rhs.dx, self.y - rhs.dy)
+
+    def __mod__(self, v: 'V'):
+        """Modulo operator, good for wrapping around into bounds"""
+        return P(self.x % v.dx, self.y % v.dy)
 
     def __str__(self):
         return f'P({self.x}, {self.y})'
@@ -41,26 +45,44 @@ class V(NamedTuple):
     def __sub__(self, v: 'V'):
         return V(self.dx - v.rd, self.dy - v.dy)
 
-    def __mul__(self, x: float):
-        return V(self.dx * x, self.dy * x)
+    def __mul__(self, rhs: 'float|V'):
+        """Scalar multiplication or dot product"""
+        if isinstance(rhs, V):
+            return self.dx * rhs.dx + self.dy * rhs.dy
+
+        return V(self.dx * rhs, self.dy * rhs)
+
+    def __rmul__(self, lhs: float):
+        return V(lhs * self.dx, lhs * self.dy)
 
     def __truediv__(self, x: float):
         return V(self.dx / x, self.dy / y)
 
-    def __floordiv__(self, x:int):
+    def __floordiv__(self, x: 'float|int'):
         return V(self.dx // x, self.dy // y)
 
     def __neg__(self):
         return V(-self.dx, -self.dy)
 
+    def __xor__(self, v: 'V'):
+        """Dot product"""
+        return self.dx * v.dy - self.dy * v.dx
+
+    def __abs__(self):
+        """Return the vector's magnitude or length"""
+        return math.sqrt(self.dx * self.dx + self.dy * self.dy)
+
     def Turn(self, degrees:float):
+        """Return a vector rotated by degrees"""
         theta = math.radians(degrees)
         s, c = math.sin(theta), math.cos(theta)
         return V(self.dx * c - self.dy * s, self.dx * s + self.dy * c)
 
-    def TurnRight(self):
+    def Right(self):
+        """Rotate right 90 degrees"""
         return V(self.dy, -self.dx)
 
-    def TurnLeft(self):
+    def Left(self):
+        """Rotate left 90 detrees"""
         return V(-self.dy, self.dx)
 
--- template/GridPoint.py
+++ template/GridPoint.py
@@ -3,6 +3,7 @@ P and V (point and vector) classes intended to be used with numpy grids. As such
 the origin is in the upper left corner, coordinates are (row, col), and row increases
 downward.
 """
+import math
 from typing import NamedTuple
 
 class P(NamedTuple):
@@ -20,6 +21,11 @@ class P(NamedTuple):
             return V(self.row - rhs.row, self.col - rhs.col)
         else:
             return P(self.row - rhs.dr, self.col - rhs.dc)
+
+    def __mod__(self, v: 'V'):
+        """Modulo operator, good for wrapping around into bounds"""
+        return P(self.row % v.dr, self.col % v.dc)
+
     def __str__(self):
         return f'P({self.row}, {self.col})'
 
@@ -29,6 +35,7 @@ class V(NamedTuple):
     """
     dr: 'int'
     dc: 'int'
+
     def __add__(self, rhs: 'V|P'):
         if isinstance(rhs, P):
             return P(self.dr + rhs.row, self.dc + rhs.col)
@@ -38,8 +45,15 @@ class V(NamedTuple):
     def __sub__(self, v: 'V'):
         return V(self.dr - v.rd, self.dc - v.dc)
 
-    def __mul__(self, x: int):
-        return V(self.dr * x, self.dc * x)
+    def __mul__(self, rhs: 'int|V'):
+        """Scalar multiplication or dot product"""
+        if isinstance(rhs, 'V'):
+            return self.dr * rhs.dr + self.dy * rhs.dy
+
+        return V(self.dr * rhs, self.dc * rhs)
+
+    def __rmul__(self, lhs: 'int'):
+        return V(lhs * self.dr, lhs * self.dc)
 
     def __floordiv__(self, x: int):
         return V(self.dr // x, self.dr // x)
@@ -47,8 +61,16 @@ class V(NamedTuple):
     def __neg__(self):
         return V(-self.dy, -self.dx)
 
-    def RightTurn(self):
+    def __xor__(self, v: 'V'):
+        """Dot product"""
+        return self.dr * v.dr + self.dc * v.dc
+
+    def __abs__(self):
+        """Return the vector's magnitude or length"""
+        return math.sqrt(self.dr * self.dr + self.dc * self.dc)
+
+    def Right(self):
         return V(self.dc, -self.dr)
 
-    def LeftTurn(self):
+    def Left(self):
         return V(-self.dc, self.dr)
