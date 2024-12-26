08e48a503baa49c4159e7f254b0cc33c77194304
Jim Atkinson <j_git@atkinsons.net>
2024-12-26 14:39:58 -0800

    Day23 solution

--- 2024/23/Day23.py
+++ 2024/23/Day23.py
@@ -2,9 +2,6 @@
 """
 <Problem description here>
 """
-import sys
-import re
-import numpy as np
 
 class Day23:
     def __init__(self):
@@ -12,7 +9,7 @@ class Day23:
 
         self.lines = []
         self.grid = None
-        
+
         self.ParseArgs()
         self.ParseInput()
 
@@ -29,31 +26,53 @@ class Day23:
         with open(self.input) as input:
             self.lines = input.read().strip().split('\n')
 
-        ########################################################################
-        # If the puzzle is not grid/map based, delete these lines.
-        gridKey = {'.': 0, '#': 1, 'O': 2}
-        self.height = len(self.lines)
-        self.width = len(self.lines[0])
-
-        self.grid = np.zeros((self.height, self.width), dtype=int)
-        for row, line in enumerate(self.lines):
-            for col, ch in enumerate(line):
-                self.grid[row, col] = gridKey[ch]
-        #
-        ########################################################################
-
 
     def Part1(self):
         answer = 0
-        return answer
+        self.netmap = {}
+        self.netset = set()
+        for line in self.lines:
+            a, b = line.split('-')
+            self.netset.add((a, b))
+            self.netset.add((b, a))
+            self.netmap.setdefault(a, []).append(b)
+            self.netmap.setdefault(b, []).append(a)
+
+        for connections in self.netmap.values():
+            connections.sort()
+
+        triples = set()
+        for host1, connected in self.netmap.items():
+            for i, host2 in enumerate(connected):
+                for j in range(i + 1, len(connected)):
+                    host3 = connected[j]
+                    if (host2, host3) in self.netset:
+                        if host1.startswith('t') or host2.startswith('t') or host3.startswith('t'):
+                            triples.add(tuple(sorted((host1, host2, host3))))
+
+        return len(triples)
+
 
     def Part2(self):
         answer = 0
+        biggestClique = set()
+        for node, connections in self.netmap.items():
+            clique = set([node])
+            for n1 in connections:
+                for n2 in clique:
+                    if (n1, n2) not in self.netset:
+                        break
+                else:
+                    clique.add(n1)
+
+            if len(clique) > len(biggestClique):
+                biggestClique = clique
+        answer = ','.join(sorted(biggestClique))
         return answer
-    
+
 if __name__ == '__main__':
     problem = Day23()
-    
+
     answer1 = problem.Part1()
     print(f'Answer 1: {answer1}')
 
